874dad48ff5d775b585e00f8f5f907c984252e7a
ck-tekton <33156025+charkour@users.noreply.github.com>
2022-08-15 12:50:21 -0400

    update logging

--- middleware.ts
+++ middleware.ts
@@ -3,13 +3,16 @@ import { NextResponse } from 'next/server';
 
 export function middleware(request: NextRequest) {
     const { pathname, searchParams } = request.nextUrl;
+    console.log(pathname, searchParams, request.cookies.get('group'))
     const isBGroup = request.cookies.get('group') === 'B' || searchParams.get('group') === 'B';
     if (isBGroup && pathname === '/') {
+	console.log('rewrite')
         return NextResponse.rewrite(new URL('/other', request.url));
     } else if (pathname === '/other') {
+	console.log('redirect');
         return NextResponse.redirect(new URL('/', request.url));
     }
-
+    console.log('else')
     return NextResponse.next();
 }
 
