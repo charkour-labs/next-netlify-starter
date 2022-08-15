37284faa547d3c1a0d4d64150f65540a92621643
ck-tekton <33156025+charkour@users.noreply.github.com>
2022-08-15 13:01:30 -0400

    fix logging

--- middleware.ts
+++ middleware.ts
@@ -3,8 +3,9 @@ import { NextResponse } from 'next/server';
 
 export function middleware(request: NextRequest) {
     const { pathname, searchParams } = request.nextUrl;
-    console.log(pathname, searchParams, request.cookies.get('group'))
-    const isBGroup = request.cookies.get('group') === 'B' || searchParams.get('group') === 'B';
+    const cookie = request.cookies.get('group');
+    console.log({ pathname: pathname.toString(), searchParams: searchParams.toString(), cookie })
+    const isBGroup = cookie === 'B' || searchParams.get('group') === 'B';
     if (isBGroup && pathname === '/') {
 	console.log('rewrite')
         return NextResponse.rewrite(new URL('/other', request.url));
