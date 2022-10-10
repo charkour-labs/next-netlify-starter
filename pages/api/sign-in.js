6b6fae8e506ca1dfebbc489caec119fc8345f98c
ck-tekton <33156025+charkour@users.noreply.github.com>
2022-10-10 15:02:27 -0400

    add max age

--- pages/api/sign-in.js
+++ pages/api/sign-in.js
@@ -3,7 +3,7 @@ import { serialize } from "cookie";
 export default function handler(req, res) {
   res.setHeader(
     "Set-Cookie",
-    serialize("customer_group", "other", { path: "/" })
+    serialize("customer_group", "other", { path: "/", maxAge: 3600 })
   );
   res.status(200).json({ name: "John Doe" });
 }
