c24ed389972663ee5fc5f15a4b078a4d0021e36d
ck-tekton <33156025+charkour@users.noreply.github.com>
2022-10-13 17:07:34 -0400

    add more logging

--- middleware.ts
+++ middleware.ts
@@ -5,6 +5,7 @@ export function middleware(request: NextRequest) {
   const { pathname, searchParams } = request.nextUrl;
   const cookie = request.cookies.get("customer_group");
   const isBGroup = cookie === "other" || searchParams.get("group") === "other";
+  const otherUrl = new URL("/other", request.url);
   console.log({
     url: request.url,
     pathname: pathname.toString(),
@@ -15,11 +16,13 @@ export function middleware(request: NextRequest) {
     locale: request.nextUrl.locale.toString(),
     defaultLocale: request.nextUrl.defaultLocale?.toString(),
     domainLocale: request.nextUrl.domainLocale?.toString(),
+    otherUrl: otherUrl.toString(),
+    rewrite: NextResponse.rewrite.toString(),
   });
 
   if (isBGroup && ["/", "/en-US"].includes(pathname.toString())) {
     console.log("rewrite");
-    return NextResponse.rewrite(new URL("/other", request.url));
+    return NextResponse.rewrite(otherUrl);
   } else if (pathname === "/other") {
     console.log("404");
     return NextResponse.rewrite(new URL("/404", request.url));
